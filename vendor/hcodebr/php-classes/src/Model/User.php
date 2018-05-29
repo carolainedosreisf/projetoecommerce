1074cb9602af2a6859415a6a1d2893db859fafed
Carolaine <carol_dos_reis@hotmail.com>
2018-05-29 09:31:08 -0300

    Curso de php 7

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,6 +11,7 @@ class User extends Model {
 	const ERROR = "UserError";
 	const ERROR_REGISTER = "UserErrorRegister";
 	const SUCCESS = "UserSucesss";
+
 	public static function getFromSession()
 	{
 		$user = new User();
