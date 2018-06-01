1c355fdbb7648a7e60646773ebfbb4b106053d84
Carolaine <carol_dos_reis@hotmail.com>
2018-06-01 08:16:02 -0300

    curso de php 7

--- site.php
+++ site.php
@@ -109,6 +109,7 @@ $app->get("/checkout", function(){
 	if (!$address->getdesstate()) $address->setdesstate('');
 	if (!$address->getdescountry()) $address->setdescountry('');
 	if (!$address->getdeszipcode()) $address->setdeszipcode('');
+	if (!$address->getdestrict()) $address->setdesnumber('');
 	$page = new Page();
 	$page->setTpl("checkout", [
 		'cart'=>$cart->getValues(),
@@ -116,6 +117,65 @@ $app->get("/checkout", function(){
 		'products'=>$cart->getProducts(),
 		'error'=>Address::getMsgError()
 	]);
+});
+$app->post("/checkout", function(){
+	User::verifyLogin(false);
+	if (!isset($_POST['zipcode']) || $_POST['zipcode'] === '') {
+		Address::setMsgError("Informe o CEP.");
+		header('Location: /checkout');
+		exit;
+	}
+	if (!isset($_POST['desaddress']) || $_POST['desaddress'] === '') {
+		Address::setMsgError("Informe o endereço.");
+		header('Location: /checkout');
+		exit;
+	}
+	if (!isset($_POST['desdistrict']) || $_POST['desdistrict'] === '') {
+		Address::setMsgError("Informe o bairro.");
+		header('Location: /checkout');
+		exit;
+	}
+	if (!isset($_POST['descity']) || $_POST['descity'] === '') {
+		Address::setMsgError("Informe a cidade.");
+		header('Location: /checkout');
+		exit;
+	}
+	if (!isset($_POST['desstate']) || $_POST['desstate'] === '') {
+		Address::setMsgError("Informe o estado.");
+		header('Location: /checkout');
+		exit;
+	}
+	if (!isset($_POST['descountry']) || $_POST['descountry'] === '') {
+		Address::setMsgError("Informe o país.");
+		header('Location: /checkout');
+		exit;
+	}
+	$user = User::getFromSession();
+	$address = new Address();
+	$_POST['deszipcode'] = $_POST['zipcode'];
+	$_POST['idperson'] = $user->getidperson();
+	$address->setData($_POST);
+	$address->save();
+	$cart = Cart::getFromSession();
+	$cart->getCalculateTotal();
+	$order = new Order();
+	$order->setData([
+		'idcart'=>$cart->getidcart(),
+		'idaddress'=>$address->getidaddress(),
+		'iduser'=>$user->getiduser(),
+		'idstatus'=>OrderStatus::EM_ABERTO,
+		'vltotal'=>$cart->getvltotal()
+	]);
+	$order->save();
+	switch ((int)$_POST['payment-method']) {
+		case 1:
+		header("Location: /order".$order->getidorder()."/pagseguro");
+		break;
+		case 2:
+		header("Location: /order".$order->getidorder()."/paypal");
+		break;
+	}
+	exit;
 });
 
 
@@ -258,7 +318,7 @@ $app->post("/profile", function(){
 	exit;
 });
 
-$app->get("/order/idorder", function($idorder){
+$app->get("/order/:idorder", function($idorder){
 	User::verifyLogin(false);
 	$order =  new Order();
 	$order->get((int)$idorder);
